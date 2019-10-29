c8b7612b65f06a24415ceddda17678d79b7fad71
Jose Pereira <jmanuelpereira2@gmail.com>
2019-10-29 19:49:30 -0400

    fixing break problem

--- Assignments/Mars Rover/mars-rover-v2/mars-rover-v2/javascripts/app.js
+++ Assignments/Mars Rover/mars-rover-v2/mars-rover-v2/javascripts/app.js
@@ -9,10 +9,14 @@ function turnLeft(rover){
 
   	switch(rover.direction){
 
-		case 'N': rover.direction='W' break;
-		case 'W': rover.direction='S' break;
-		case 'S': rover.direction='E' break;
-		case 'E': rover.direction='N' break;
+		case 'N': rover.direction='W' 
+		break;
+		case 'W': rover.direction='S' 
+		break;
+		case 'S': rover.direction='E' 
+		break;
+		case 'E': rover.direction='N' 
+		break;
 	}
   console.log("turnLeft was called!");
 }
@@ -21,10 +25,14 @@ function turnRight(rover){
 
   	switch(rover.direction){
 
-		case 'N': rover.direction='E' break;
-		case 'E': rover.direction='S' break;
-		case 'S': rover.direction='W' break;
-		case 'W': rover.direction='N' break;
+		case 'N': rover.direction='E' 
+		break;
+		case 'E': rover.direction='S' 
+		break;
+		case 'S': rover.direction='W' 
+		break;
+		case 'W': rover.direction='N' 
+		break;
 	}
   console.log("turnRight was called!")
 }
