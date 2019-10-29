1a012ef24d9fdc663fb82f5808e02deff14b3410
Jose Pereira <jmanuelpereira2@gmail.com>
2019-10-29 19:35:09 -0400

    updating repo

--- Assignments/Mars Rover/mars-rover-v2/mars-rover-v2/javascripts/app.js
+++ Assignments/Mars Rover/mars-rover-v2/mars-rover-v2/javascripts/app.js
@@ -1,20 +1,98 @@
-// Rover Object Goes Here
-// ======================
+const rover = {
+   direction: "N",
+   positionX: 0,
+   positionY: 0,
+   travelLog: []
+};
 
-// ======================
 function turnLeft(rover){
+
+  	switch(rover.direction){
+
+		case 'N': rover.direction='W' break;
+		case 'W': rover.direction='S' break;
+		case 'S': rover.direction='E' break;
+		case 'E': rover.direction='N' break;
+	}
   console.log("turnLeft was called!");
 }
 
 function turnRight(rover){
-  console.log("turnRight was called!");
+
+  	switch(rover.direction){
+
+		case 'N': rover.direction='E' break;
+		case 'E': rover.direction='S' break;
+		case 'S': rover.direction='W' break;
+		case 'W': rover.direction='N' break;
+	}
+  console.log("turnRight was called!")
 }
 
-function moveForward(rover){
-  console.log("moveForward was called")
-}
+//Function Forward
+  function moveForward(rover){
+   switch(rover.direction){
+  	case 'N': 
+  	    if(rover.positionY <= 0 || rover.positionY > 10){
+          console.log("Rover reached limit grid")
+  	    }else{
+  	      rover.positionY-=1
+  	    }
+  	   break;
+  	
+  	case 'E':
+  	   if(rover.positionX < 0 || rover.positionX > 10){
+          console.log("Rover reached limit grid")
+  	    }else{   
+  	      rover.positionX+=1
+  	    }
+  	    break;
+  	
+	  case 'S':
+	     if(rover.positionY < 0 || rover.positionY > 10){
+          console.log("Rover reached limit grid")
+	     }else {
+	        rover.positionY+=1
+	     }
+	    break;
+	    
+	  case 'W':
+	     if(rover.positionX < 0 || rover.positionX > 10){
+          console.log("Rover reached limit grid")
+	     }else {
+	          rover.positionX-=1
+	     }
+	    break;
+   }
+   
+   console.log("moveForward was called")
+   console.log("Rover is here:" + [rover.positionX, rover.positionY])
+  }
 
-const rover = {
- direction = "N",
 
+  function commandsRover(rover){
+   let route = prompt("Insert the commands to move the Rover: f: forward, r: right, l: left");
+
+   if(route.indexOf('f') >= 0 || route.indexOf('r') >= 0 || route.indexOf('l') >= 0 ){
+   let newRoute = route.split("");
+   console.log("Rover's route: " + route);
+	 for(let i = 0;  i < newRoute.length; i++){
+	   
+      switch(route[i]){
+		  case 'l': turnLeft(rover); break;
+		  case 'r': turnRight(rover); break;
+		  case 'f': moveForward(rover); break;
+	   }
+	   
+      let position = [rover1.positionX, rover1.positionY];
+      rover1.travelLog.push(position);
+      console.log("Rover's current location: " + position);
+  
+	  }
+	 
+   }else {
+     alert("Enter a valid command!!");
+     commandsRover(rover1);
+   }
 }
+commandsRover(rover1);
